161e53bfe67c01e99ff0a71648b9684a7e5931f9
alejograjal <alejandro.grajal.s@gmail.com>
2024-09-08 21:46:49 -0600

    Configuration setup

--- BaseReservation/BaseReservation.Application/Configuration/Configuration.cs
+++ BaseReservation/BaseReservation.Application/Configuration/Configuration.cs
@@ -1,4 +1,8 @@
 using BaseReservation.Application.Profiles;
+using BaseReservation.Application.Services.Implementations;
+using BaseReservation.Application.Services.Implementations.Authorization;
+using BaseReservation.Application.Services.Interfaces;
+using BaseReservation.Application.Services.Interfaces.Authorization;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace BaseReservation.Application.Configuration;
@@ -11,7 +15,39 @@ public static class Configuration
     /// <param name="services">Service collection configuration</param>
     public static void ConfigureApplication(this IServiceCollection services)
     {
+        services.AddTransient<IServiceUsuario, ServiceUsuario>();
+        services.AddTransient<IServiceProducto, ServiceProducto>();
+        services.AddTransient<IServiceRol, ServiceRol>();
+        services.AddTransient<IServiceDetalleFactura, ServiceDetalleFactura>();
+        services.AddTransient<IServiceFactura, ServiceFactura>();
+        services.AddTransient<IServiceSucursal, ServiceSucursal>();
+        services.AddTransient<IServiceReserva, ServiceReserva>();
+        services.AddTransient<IServiceServicio, ServiceServicio>();
+        services.AddTransient<IServiceReservaPregunta, ServiceReservaPregunta>();
+        services.AddTransient<IServiceUnidadMedida, ServiceUnidadMedida>();
+        services.AddTransient<IServiceCategoria, ServiceCategoria>();
+        services.AddTransient<IServiceHorario, ServiceHorario>();
+        services.AddTransient<IServiceTipoServicio, ServiceTipoServicio>();
+        services.AddTransient<IServiceProvincia, ServiceProvincia>();
+        services.AddTransient<IServiceCanton, ServiceCanton>();
+        services.AddTransient<IServiceDistrito, ServiceDistrito>();
+        services.AddTransient<IServiceFeriado, ServiceFeriado>();
+        services.AddTransient<IServiceSucursalFeriado, ServiceSucursalFeriado>();
+        services.AddTransient<IServiceSucursalHorario, ServiceSucursalHorario>();
+        services.AddTransient<IServiceSucursalHorarioBloqueo, ServiceSucursalHorarioBloqueo>();
+        services.AddTransient<IServiceInventario, ServiceInventario>();
+        services.AddTransient<IServicePedido, ServicePedido>();
+        services.AddTransient<IServiceDetalleReserva, ServiceDetalleReserva>();
+        services.AddTransient<IServiceCliente, ServiceCliente>();
+        services.AddTransient<IServiceInventarioProducto, ServiceInventarioProducto>();
+        services.AddTransient<IServiceInventarioProductoMovimiento, ServiceInventarioProductoMovimiento>();
+        services.AddTransient<IServiceTipoPago, ServiceTipoPago>();
+        services.AddTransient<IServiceImpuesto, ServiceImpuesto>();
+        services.AddTransient<IServiceProveedor, ServiceProveedor>();
+        services.AddTransient<IServiceUsuarioSucursal, ServiceUsuarioSucursal>();
 
+        services.AddScoped<IServiceUserContext, ServiceUserContext>();
+        services.AddScoped<IServiceUserAuthorization, ServiceUserAuthorization>();
     }
 
     /// <summary>
