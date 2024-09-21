5ac327a29b434e126e76fb91670e1b32b849c315
Alejandro Grajal <63889237+alejograjal@users.noreply.github.com>
2024-09-21 12:38:58 -0600

    Feat/ delete costumer (#23)
    * Modified Client Services and Repository
    
    * Solving minor fixes
    
    ---------
    
    Co-authored-by: BrayanAA <boaguirrea@est.utn.ac.cr>

--- BaseReservation/BaseReservation.Application/Services/Implementations/ServiceCliente.cs
+++ BaseReservation/BaseReservation.Application/Services/Implementations/ServiceCliente.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using BaseReservation.Application.Comunes;
 using BaseReservation.Application.ResponseDTOs;
 using BaseReservation.Application.Services.Interfaces;
 using BaseReservation.Infrastructure.Repository.Interfaces;
@@ -8,9 +9,25 @@ namespace BaseReservation.Application.Services.Implementations;
 public class ServiceCliente(IRepositoryCliente repository, IMapper mapper) : IServiceCliente
 {
     /// <inheritdoc />
+    public async Task<bool> DeleteCustomerAsync(short id)
+    {
+        if (!await repository.ExistsCustomerAsync(id)) throw new NotFoundException("Cliente no encontrado.");
+        return await repository.DeleteCustomerAsync(id);
+    }
+
+    /// <inheritdoc />
+    public async Task<ResponseClienteDto?> FindByIdAsync(short id)
+    {
+        var customer = await repository.FindByIdAsync(id);
+        if (customer == null) throw new NotFoundException("Cliente no encontrado.");
+
+        return mapper.Map<ResponseClienteDto>(customer);
+    }
+
+    /// <inheritdoc />
     public async Task<ICollection<ResponseClienteDto>> ListAllAsync()
     {
-        var clientes = await repository.ListAllAsync();
-        return mapper.Map<ICollection<ResponseClienteDto>>(clientes);
+        var customers = await repository.ListAllAsync();
+        return mapper.Map<ICollection<ResponseClienteDto>>(customers);
     }
 }
--- BaseReservation/BaseReservation.Infrastructure/Repository/Interfaces/IRepositoryCliente.cs
+++ BaseReservation/BaseReservation.Infrastructure/Repository/Interfaces/IRepositoryCliente.cs
@@ -5,8 +5,36 @@ namespace BaseReservation.Infrastructure.Repository.Interfaces;
 public interface IRepositoryCliente
 {
     /// <summary>
-    /// Get list of all existing customer
+    /// Get list of all existing customer.
     /// </summary>
-    /// <returns>ICollection of Categoria</returns>
+    /// <returns>ICollection of Cliente.</returns>
     Task<ICollection<Cliente>> ListAllAsync();
+
+    /// <summary>
+    ///  Get exact customer according to id, if not, get null
+    /// </summary>
+    /// <param name="id">The ID of the customer.</param>
+    /// <returns>Cliente</returns>
+    Task<Cliente?> FindByIdAsync(short id);
+
+    /// <summary>
+    /// Updates an existing customer record.
+    /// </summary>
+    /// <param name="customer">The customer to update.</param>
+    /// <returns>Cliente</returns>
+    Task<Cliente> UpdateCostumerAsync(Cliente customer);
+
+    /// <summary>
+    /// Deletes a customer based on the provided Id.
+    /// </summary>
+    /// <param name="id">Id of the customer to delete.</param>
+    /// <returns>True if successful, otherwise false.</returns>
+    Task<bool> DeleteCustomerAsync(short id);
+
+    /// <summary>
+    /// Checks if a costumer exists by its ID
+    /// </summary>
+    /// <param name="id">The Id of the customer.</param>
+    /// <returns>True if customer exist, otherwise false.</returns>
+    Task<bool> ExistsCustomerAsync(short id);
 }
