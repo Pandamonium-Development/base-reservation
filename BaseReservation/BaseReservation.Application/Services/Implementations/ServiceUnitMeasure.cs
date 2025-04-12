e011db1f7962550b14eccd3d7163d4b83a477c86
alejograjal <alejandro.grajal.s@gmail.com>
2025-04-12 13:01:46 -0600

    fix: adjust unnecessary method

--- BaseReservation/BaseReservation.Application/Services/Implementations/ServiceUnitMeasure.cs
+++ BaseReservation/BaseReservation.Application/Services/Implementations/ServiceUnitMeasure.cs
@@ -42,17 +42,6 @@ public class ServiceUnitMeasure(ICoreService<UnitMeasure> coreService, IMapper m
     }
 
     /// <inheritdoc />
-    public async Task<ResponseUnitMeasureDto> FindByIdAsync(byte id)
-    {
-        if (!await coreService.UnitOfWork.Repository<UnitMeasure>().ExistsAsync(id)) throw new NotFoundException("Unidad de medida no encontrada.");
-
-        var spec = new BaseSpecification<UnitMeasure>(x => x.Id == id);
-        var unitMeasure = await coreService.UnitOfWork.Repository<UnitMeasure>().FirstOrDefaultAsync(spec);
-
-        return mapper.Map<ResponseUnitMeasureDto>(unitMeasure);
-    }
-
-    /// <inheritdoc />
     public async Task<ResponseUnitMeasureDto> FindByIdAsync(long id)
     {
         var spec = new BaseSpecification<UnitMeasure>(x => x.Id == id);
